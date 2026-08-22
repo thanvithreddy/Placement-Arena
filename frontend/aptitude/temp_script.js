757840b005caef7aede670e802cb5e00eb35f8d7
Thanvith Reddy <thanvith.vv@gmail.com>
2026-08-22 22:22:22 +0530

    patchy

---
+++ frontend/aptitude/temp_script.js
@@ -0,0 +1,14 @@
+const aptitudeTopics = [];
+
+    window.MathJax = {
+      tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        processEscapes: true
+      },
+      options: {
+        ignoreHtmlClass: 'tex2jax_ignore',
+        processHtmlClass: 'tex2jax_process'
+      }
+    };
+    
